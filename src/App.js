f0c4b099fdc8e2977a5683b7c6f64df4c3253d77
Vishal Dhudum <v.dhudum@avendata.com>
2020-11-27 21:00:50 +0530

    Mozilla pdf.js implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  let viewerRef = React.useRef(null)
+
+  React.useEffect(() => {
+    renderIframe();
+  }, []);
+
+  function renderIframe() {
+    const element = viewerRef.current;
+    const iframe = document.createElement('iframe');
+
+    iframe.src = `/pdfjs/web/viewer.html?file=/sample1.pdf#pagemode=thumbs`; // version 2.5.207
+
+    iframe.width = '100%';
+    iframe.height = '100%';
+    iframe.style.border = 0;
+    element.appendChild(iframe);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div ref={viewerRef} id='viewer' style={{height: '100vh'}}/>
     </div>
   );
 }
